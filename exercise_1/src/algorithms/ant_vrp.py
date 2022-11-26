add count to self define prepare to avoid wrong initialization define candidate lists skip calculation if only depot is left bug fix in fitness add depot to path

exercise_1/src/algorithms/ant_vrp.py
@@ -9,30 +9,54 @@ class AntVRP(Ant):
         super().__init__(weights, nodes, **kwargs)
         self.demand = demand
         self.max_capacity = capacity
-        self.capacity = self.max_capacity
-        self.start_node = self.nodes[count % self.nodes.shape[0]] # one ant per node if #ants == nodes
+        self.count = count
+        self.prepare(probabilities=None)
+
+    def prepare(self, probabilities):
+        self.pheromones_segregated = np.zeros(self.weights.shape)
+        self.probabilities = probabilities
+
+        self.start_node = self.nodes[self.count % self.nodes.shape[0]] # one ant per node if #ants == nodes
         self.current_node = self.start_node
-        self.path: List = [self.current_node]
+        self.capacity = self.max_capacity - self.demand[self.current_node]
+        if self.current_node == 0:
+            self.path: List = [self.current_node]
+        else:
+            self.path: List = [0, self.current_node]
         self.nodes_not_visited: np.array = self.nodes[self.nodes != self.current_node]  # all nodes except start node
         self.fitness: float = self.weights[0][self.current_node] # all routes must start from depot
 
     # move ant from one node to another based on the transition probabilities
-    def move(self) -> None:
+    def move(self, **kwargs) -> None:
+
         # use probabilitys at current node -> only consider not visited nodes + depot
         probabilities_at_current_node = self.probabilities[self.current_node]
 
-        nodes_not_visited_next = self.nodes_not_visited[self.demand[self.nodes_not_visited] <= self.capacity]
-        if not 0 in nodes_not_visited_next:
-            nodes_not_visited_next = np.insert(nodes_not_visited_next,0,0)
+        # use candidates to only consider nearest nodes
+        candidate_ratio = kwargs.get("candidate_ratio", 1)
+        candidate_index = int(candidate_ratio * len(self.nodes))
+        distances = np.argsort(self.weights[self.current_node][self.nodes_not_visited])[:candidate_index]
+        candidates = self.nodes_not_visited[distances]
 
-        probabilities_not_visited = probabilities_at_current_node[nodes_not_visited_next]
-        probabilities_not_visited /= probabilities_not_visited.sum()  # rescale to norm 1
-        selected_node = np.random.choice(nodes_not_visited_next, p=probabilities_not_visited)
+        # only consider nodes that fullfil the constraint
+        nodes_not_visited_next = candidates[self.demand[candidates] <= self.capacity]
+        if not 0 in nodes_not_visited_next:
+            nodes_not_visited_next = np.insert(nodes_not_visited_next,0,0) # always be able to go back to the depot
+        
+        if len(nodes_not_visited_next) == 1: # only depot left
+            selected_node = nodes_not_visited_next[0]
+        else:
+            probabilities_not_visited = probabilities_at_current_node[nodes_not_visited_next]
+            probabilities_not_visited /= probabilities_not_visited.sum()  # rescale to norm 1
+         
+            selected_node = np.random.choice(nodes_not_visited_next, p=probabilities_not_visited)
+
+        self.pheromones_segregated[self.current_node][selected_node] += self.pheromone_intensity
+        self.nodes_not_visited = self.nodes_not_visited[self.nodes_not_visited != selected_node]
+        
         # update values
         self.path.append(selected_node)
         self.fitness += self.weights[self.current_node][selected_node]
-        self.pheromones_segregated[self.current_node][selected_node] = self.pheromone_intensity
-        self.nodes_not_visited = self.nodes_not_visited[self.nodes_not_visited != selected_node]
         self.current_node = selected_node
         self.capacity -= self.demand[selected_node]
 
@@ -42,10 +66,10 @@ class AntVRP(Ant):
 
         # move back to start if finished
         if self.has_finished():
-            self.fitness += self.weights[self.current_node][self.start_node]
-            self.pheromones_segregated /= self.fitness  # self.weights[self.current_node][selected_node]
-            self.current_node = self.start_node
-
+            self.fitness += self.weights[self.current_node][0] # close loop
+            self.path.append(0)
+            self.pheromones_segregated /= self.fitness  
+            
     # determine when ant is finished
     def has_finished(self) -> bool:
         return len(self.nodes_not_visited) == 0
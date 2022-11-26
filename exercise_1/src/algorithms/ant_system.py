9d04daf7b1c5f56df4b4a27c5748fc1b6e2bf994
Laurenz Ruzicka <laurenz_ruzicka@gmx.at>
2022-11-26 15:49:04 +0100

    added savings_function visibility changed type hint from evaporation_coefficient to float store elitist added kwargs to run and _update_pheromons capped probabilities added best, all, elitist trail updates

--- exercise_1/src/algorithms/ant_system.py
+++ exercise_1/src/algorithms/ant_system.py
@@ -12,46 +12,70 @@ class AntSystem():
         self.problem_instance: np.ndarray = problem_instance
         self.dimension: int = self.problem_instance.shape[0]
         self.nodes: np.array = np.arange(self.dimension)
-        self.visibility = np.maximum(np.power(problem_instance.astype('float64'), -1, where=(problem_instance != 0.0)), 0)  # 1/n_ij
+
+        visibility_choice = kwargs.get("visibility", "distance")
+        if visibility_choice == "distance":
+            self.visibility = np.maximum(np.power(problem_instance.astype('float64'), -1, where=(problem_instance != 0.0)), 0)  # 1/n_ij
+        elif visibility_choice == "saving_function":
+            # n_ij = d_i0 + d_0j - g* d_ij + f * |d_i0 - d_0j|
+            f = kwargs.get("f", 2)
+            g = kwargs.get("g", 2)
+            di0 = np.zeros_like(problem_instance)
+            d0j = np.zeros_like(problem_instance)
+            di0[:,:] = problem_instance[:,0]
+            di0 = di0.T
+
+            d0j[:,:] = problem_instance[0,:]
+
+            self.visibility = di0 + d0j - problem_instance.astype('float64') * g + f * np.absolute(di0 - d0j)
 
         self.alpha = kwargs.get('alpha', 1)
         self.beta = kwargs.get('beta', 1)
         self.time = kwargs.get('time', 100)
-        self.evaporation_coefficient: int = kwargs.get('evaporation_coefficient', 0.7)
+        self.evaporation_coefficient: float = kwargs.get('evaporation_coefficient', 0.7)
         self.number_of_ants = kwargs.get('number_of_ants', 20)
         self.debug_mode = kwargs.get('debug_mode', False)
 
         self.trail_intensity: np.ndarray = np.full(self.problem_instance.shape, 0.001)  # tau
         self.best_fitness_at_time_point: List[float] = []
         self.best_path: List[int] = []
+
         # vrp constraints
         self.demand = kwargs.get('demand', None)
         self.capacity = kwargs.get('capacity', None)
 
-    def run(self) -> None:
-        ants = [self.ant_class(self.problem_instance, self.nodes, count=i, demand=self.demand, capacity=self.capacity) for i in range(self.number_of_ants)]
+    def run(self, **kwargs) -> None:
+        ants = [self.ant_class(self.problem_instance, self.nodes, count=i, demand=self.demand, capacity=self.capacity, **kwargs) for i in range(self.number_of_ants)]
         # iterate time points
         for t in tqdm(np.arange(self.time), desc='Time Point: '):
             # recalculate transition probabilities because of pheromone updates
             transition_probabilities = self._caluclate_transition_probabilities()
             # find soultion for all ants
-            fitness_all_ants = []
+            self._fitness_all_ants = []
             for ant in ants:
                 ant.prepare(transition_probabilities)
                 while not ant.has_finished():
-                    ant.move()
-                fitness_all_ants.append(ant.fitness)
+                    ant.move(**kwargs)
+                self._fitness_all_ants.append(ant.fitness)
             # evaluate ants
-            top_ant = np.argsort(fitness_all_ants)[0]
-            self.best_fitness_at_time_point.append(fitness_all_ants[top_ant])
-            self.best_path = ants[top_ant].path
+            self._top_ant = np.argsort(self._fitness_all_ants)[0]
+
+            # store historical best path
+            if t == 0 or self._fitness_all_ants[self._top_ant] < np.min(self.best_fitness_at_time_point):
+                self.best_path = ants[self._top_ant].path
+                self.best_pheromones = ants[self._top_ant].pheromones_segregated
+
+            self.best_fitness_at_time_point.append(self._fitness_all_ants[self._top_ant])
+
             # print info
             if self.debug_mode:
                 print(f'Time Point: {t + 1}')
-                print(f'Top fitness: {fitness_all_ants[top_ant]}')
-                print(f'Solution: {ants[top_ant].path}')
+                print(f'Top fitness: {self._fitness_all_ants[self._top_ant]}')
+                print(f'Trail intensity: {self.trail_intensity}')
+                print(f'Visibility: {self.visibility}')
+                print(f'Solution: {ants[self._top_ant].path}')
             # update pheromones
-            self._update_pheromones(ants)
+            self._update_pheromones(ants, **kwargs)
 
     def fitness_df(self) -> pd.DataFrame:
         return pd.DataFrame({
@@ -73,15 +97,39 @@ class AntSystem():
                 )
         # denominator -> devide by sum in row -> get probabilities
         probabilities /= probabilities.sum(axis=1, keepdims=True)
-        return probabilities
+        return np.maximum(0,probabilities)
 
     # apply update rule to update trail_intensity matrix after iteration
-    def _update_pheromones(self, ants):
-        pheromone_updates_all_ants = np.zeros(self.problem_instance.shape)
+    def _update_pheromones(self, ants, **kwargs):
+
         # collect pheromons over all ants
-        for ant in ants:
-            pheromone_updates_all_ants += ant.pheromones_segregated
+        if kwargs.get("trail_update", "all") == "all":
+            pheromone_updates = np.zeros(self.problem_instance.shape)
+            
+            for ant in ants:
+                pheromone_updates += ant.pheromones_segregated
+
+        # collect pheromons only from best ant    
+        elif kwargs.get("trail_update", "all") == "best":
+            pheromone_updates = ants[self._top_ant].pheromones_segregated * len(ants) # scale pheromone intensity
+        
+        # collect pheromons from best_rate * len(ants) ants + ellitist ant
+        elif kwargs.get("trail_update", "all") == "elitist":
+            best_rate = kwargs.get("best_rate", 0.1)
+            best_num = best_rate * len(ants)
+
+            # use pheromones from best ants
+            pheromone_updates = np.zeros_like(self.problem_instance)
+            ranked_ants = np.argsort(self._fitness_all_ants)
+            for e in range(int(best_num)-1):
+                pheromone_updates += ants[ranked_ants[e]].pheromones_segregated 
+
+            # use elitist ant
+            pheromone_updates += self.best_pheromones * (len(ants) - best_num)
+
         # evaporate pheromones
         self.trail_intensity *= (1 - self.evaporation_coefficient)
+        
         # add newly segregated pheromones
-        self.trail_intensity += pheromone_updates_all_ants
+        self.trail_intensity += pheromone_updates
+       

33fddfea439c69ab9b5e4fcfec844de0605335a4
MarkusKiesel93 <markus.kiesel@tuta.io>
2022-11-27 12:20:56 +0100

    directly return optimal fitness for tsp problem loader

--- exercise_1/src/problem_loaders/tsp_loader.py
+++ exercise_1/src/problem_loaders/tsp_loader.py
@@ -12,16 +12,29 @@ class TSPLoader():
         self.DATA_PATH = config.DATA_PATH / 'tsp'
         self.porblem_name = problem_name
 
-    def load_problem_instance(self) -> Tuple[np.ndarray, np.array]:
+    def load_problem_instance(self) -> Tuple[np.ndarray, float]:
         method_name = f'_load_{self.porblem_name}'
         try:
             method = getattr(self, method_name)  # gets the function by method name
-            problem_instance, best_solution = method()
+            problem_instance, optimal_fitness = method()
             assert problem_instance.diagonal().sum() == 0.0  # weights to same node have to be 0
             assert problem_instance.shape[0] == problem_instance.shape[1]  # symmetric matrix
-            return problem_instance, best_solution
+            return problem_instance, optimal_fitness
         except AttributeError:
             raise NotImplementedError(f'Metod {method_name} not implemented')
+
+    def _fitness(self, problem_instance: np.ndarray, solution: np.array) -> float:
+        weights = []
+        for i in range(len(solution)):
+            from_node = solution[i]
+            # current node to next node
+            if i < len(solution) - 1:
+                to_node = solution[i + 1]
+            # last node to first node
+            else:
+                to_node = solution[0]
+            weights.append(problem_instance[from_node][to_node])
+        return sum(weights)
 
     def _load_FIVE(self):
         path = self.DATA_PATH / 'FIVE'
@@ -29,8 +42,9 @@ class TSPLoader():
                          sep='  ',
                          header=None,
                          engine='python')
+        problem_instance = df.values
         best_solution = self._load_values_by_line(path / 'five_s.txt')
-        return df.values, best_solution
+        return problem_instance, self._fitness(problem_instance, best_solution)
 
     def _load_GR17(self):
         path = self.DATA_PATH / 'GR17'
@@ -38,8 +52,9 @@ class TSPLoader():
         with open(path / 'gr17_d.txt', 'r') as file:
             for line in file.readlines():
                 edge_valus.append([int(value) for value in line.rstrip().split(' ') if value != ''])
+        problem_instance = np.array(edge_valus)
         best_solution = self._load_values_by_line(path / 'gr17_s.txt')
-        return np.array(edge_valus), best_solution[:-1]
+        return problem_instance, self._fitness(problem_instance, best_solution[:-1])
 
     def _load_DANTZIG42(self):
         path = self.DATA_PATH / 'DANTZIG42'
@@ -47,7 +62,7 @@ class TSPLoader():
         with open(path / 'dantzig42_d.txt', 'r') as file:
             for line in file.readlines():
                 edge_valus.append([int(value) for value in line.rstrip().split(' ') if value != ''])
-        return np.array(edge_valus), None  # no best solution found but known value (699)
+        return np.array(edge_valus), 699  # no best solution found but known value
 
     def _load_ATT48(self):
         path = self.DATA_PATH / 'ATT48'
@@ -55,8 +70,9 @@ class TSPLoader():
         with open(path / 'att48_d.txt', 'r') as file:
             for line in file.readlines():
                 edge_valus.append([int(value) for value in re.split(r'[\s,]+', line) if value != ''])
+        problem_instance = np.array(edge_valus)
         best_solution = self._load_values_by_line(path / 'att48_s.txt')
-        return np.array(edge_valus), best_solution[:-1]
+        return problem_instance, self._fitness(problem_instance, best_solution[:-1])
 
     def _load_values_by_line(self, path) -> np.array:
         values = []

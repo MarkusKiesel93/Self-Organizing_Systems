2aa04027278657ffdab73d33e8e80bef8aebb84b
MarkusKiesel93 <markus.kiesel@tuta.io>
2022-11-27 20:55:01 +0100

    bugfix if probabilities get too small

--- exercise_1/src/algorithms/ant_tsp.py
+++ exercise_1/src/algorithms/ant_tsp.py
@@ -10,10 +10,16 @@ class AntTSP(Ant):
     # move ant from one node to another based on the transition probabilities
     def move(self, *args, **kwargs) -> None:
         # use probabilitys at current node -> only consider not visited nodes
-        probabilities_at_current_node = self.probabilities[self.current_node]
-        proabilities_not_visited = probabilities_at_current_node[self.nodes_not_visited]
-        proabilities_not_visited /= proabilities_not_visited.sum()  # rescale to norm 1
-        selected_node = np.random.choice(self.nodes_not_visited, p=proabilities_not_visited)
+        if self.nodes_not_visited.size > 1:
+            probabilities_at_current_node = self.probabilities[self.current_node]
+            proabilities_not_visited = probabilities_at_current_node[self.nodes_not_visited]
+            if proabilities_not_visited.sum() > 0.0:
+                proabilities_not_visited /= proabilities_not_visited.sum()  # rescale to norm 1
+                selected_node = np.random.choice(self.nodes_not_visited, p=proabilities_not_visited)
+            else:
+                selected_node = np.random.choice(self.nodes_not_visited)
+        else:
+            selected_node = self.nodes_not_visited[0]
         # update values
         self.path.append(selected_node)
         self.fitness += self.weights[self.current_node][selected_node]
